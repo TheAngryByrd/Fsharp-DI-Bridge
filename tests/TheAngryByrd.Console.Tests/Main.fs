8c728df9cd33cea36c4da78d173f6bfa37a21109
Jimmy Byrd <jimmybyrd@gmail.com>
2022-01-26 14:12:37 -0500

    adds tests project

---
+++ tests/TheAngryByrd.Console.Tests/Main.fs
@@ -0,0 +1,6 @@
+﻿module TheAngryByrd.Console.Tests
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
---
+++ tests/TheAngryByrd.Console.Tests/Sample.fs
@@ -0,0 +1,13 @@
+module Tests
+
+open Expecto
+
+[<Tests>]
+let tests =
+  testList "samples" [
+    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
+      let subject = true
+      Expect.isTrue subject "I compute, therefore I am."
+
+
+  ]
