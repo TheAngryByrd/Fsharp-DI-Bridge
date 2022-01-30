96b3cea368ab4f754719affde3a3601746a3580d
Jimmy Byrd <jimmybyrd@gmail.com>
2022-01-30 12:17:05 -0500

    Showing tests using Moq

--- tests/TheAngryByrd.Console.Tests/Sample.fs
+++ tests/TheAngryByrd.Console.Tests/Sample.fs
@@ -2,6 +2,7 @@ module Tests
 
 open Expecto
 open TheAngryByrd.Console
+open System
 
 type IGetActorRequirements = 
   inherit IProvideLoggers 
@@ -21,16 +22,18 @@ module IGetActorRequirements =
 
 module ILogger =
   open Microsoft.Extensions.Logging
+  open System
+
   let create () = {
     new ILogger with
     override this.BeginScope(state: 'TState): System.IDisposable = 
         failwith "Not Implemented"
     override this.IsEnabled(logLevel: LogLevel): bool = 
         false
-    override this.Log(logLevel: LogLevel, eventId: EventId, state, ``exception``: exn, formatter: System.Func<'T,exn,string>): unit = 
-        printfn "state: %A" state
+    override this.Log(logLevel: LogLevel, eventId: EventId, state, ``exception``: exn, formatter: System.Func<_,exn,string>): unit = 
         ()
   }
+
 module IDistributedCache =
   open Microsoft.Extensions.Caching.Distributed
   open System.Threading.Tasks
@@ -57,7 +60,8 @@ module IDistributedCache =
 open System.Threading.Tasks
 open System
 type IIWrapDapper =
-//querySingleIAsync : FormattableString -> option<System.Data.IDbTransaction> -> option<int> -> option<System.Data.CommandType> -> option<System.Threading.CancellationToken> -> Task<'a>)
+  // Passing functions to implement members here doesn't really work. See https://stackoverflow.com/questions/70886932/implementing-an-interface-with-a-generic-parameter-on-a-f-record
+  // So this is a limited approach of being able to configure interfaces in tests easily
   static member create (?querySingleIAsync : FormattableString -> Task<obj>) = {
     new IWrapDapper with
     override this.DeleteAsync(sql: Dapper.FSharp.DeleteQuery, transaction: System.Data.IDbTransaction option, commandTimeout: int option, cancellationToken: System.Threading.CancellationToken option): System.Threading.Tasks.Task<int> = 
@@ -88,11 +92,15 @@ type IIWrapDapper =
     member this.UpdateAsync<'T>(sql: Dapper.FSharp.UpdateQuery<'T>, transaction: System.Data.IDbTransaction option, commandTimeout: int option, cancellationToken: System.Threading.CancellationToken option): System.Threading.Tasks.Task<int> = 
         failwith "Not Implemented" 
   }
+open Moq
+open Microsoft.Extensions.Logging
+open TheAngryByrd.Console.ActorRepository
+open Microsoft.Extensions.Caching.Distributed
 
 [<Tests>]
 let tests =
   testList "samples" [
-    testCaseAsync "Get Actor Demo" <| async {
+    testCaseAsync "Get Actor With Object Expressions" <| async {
       let logger = fun name -> ILogger.create()
       let distributedCache = IDistributedCache.create()
       let actor : ActorRepository.Actor = {
@@ -102,8 +110,7 @@ let tests =
         last_update = DateTime.Now
       }
       let expected = Ok actor
-      let querySingleIAsync query = task {
-        printfn "%A" query
+      let querySingleIAsync _ = task {
         return box actor
       }
       let database = IIWrapDapper.create(querySingleIAsync)
@@ -111,9 +118,36 @@ let tests =
       let env = IGetActorRequirements.create logger distributedCache database utcNow
 
       let! actual = App.getActor 3 env
+      Expect.equal actual expected ""
+    }
+    testCaseAsync "Get Actor With Mocks" <| async {
+      let actor : ActorRepository.Actor = {
+        first_name = "William"
+        actor_id = 1701
+        last_name = "Shatner"
+        last_update = DateTime.Now
+      }
+      let expected = Ok actor
+      
+      let envMock = Mock<IGetActorRequirements>()
+      let logger = Mock<ILogger>()
+      envMock.Setup(fun env -> env.CreateLogger(It.IsAny<string>())).Returns(logger.Object) |> ignore
+      let database = Mock<IWrapDapper>()
+      let querySingleIAsync = task {
+        printfn "%A" query
+        return actor
+      }
+      // F# optional parameters and Moq are weird. You have to specify all named parameters and use the `?paramName` syntax otherwise Moq doesn't recognize it being called
+      database.Setup(fun env -> env.QuerySingleIAsync<Actor>(It.IsAny<_>(),?transaction=It.IsAny<_>(),?commandTimeout=It.IsAny<_>(),?commandType=It.IsAny<_>(),?cancellationToken=It.IsAny<_>())).Returns(querySingleIAsync) |> ignore
+      envMock.Setup(fun env -> env.Database).Returns(database.Object) |> ignore
+      let distributedCache = Mock<IDistributedCache>()
+      envMock.Setup(fun env -> env.DistributedCache).Returns(distributedCache.Object) |> ignore
+
+      envMock.Setup(fun env -> env.UtcNow).Returns(DateTime.UtcNow) |> ignore
+
+      let! actual = App.getActor 3 envMock.Object
 
       Expect.equal actual expected ""
-      ()
     }
 
   ]
